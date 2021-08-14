6c4d30cc2bd5e905eae6a8259e41b4b05c0e2d84
Brady Bellini <bbellini@pm.me>
2021-08-13 22:20:25 -0700

    Update api.ts
    still testing some things and getting stuff figured out

--- src/api.ts
+++ src/api.ts
@@ -1,37 +1,35 @@
 class HTTPError extends Error {}
 export class Tiingo {
-  private base_url: string;
+  private _base_url: string;
   public headers: { [name: string]: string };
-  private apiKey: string;
+  public apiKey: string;
 
   constructor(apiKey: string) {
-    this.base_url = "https://api.tiingo.com/tiingo";
+    this._base_url = "https://api.tiingo.com/tiingo";
     this.apiKey = `&token=${apiKey}`;
     this.headers = {
       "Authorization": `Token ${apiKey}`,
       "Content-Type": "application/json",
     };
   }
-
-  eod(ticker: string) {
+// work on constructing the request with the optional variables
+  // possibly add logic for sorting
+  // TODO add error catching maybe
+  async eod(
+    ticker: string,
+    startDate?: string,
+    endDate?: string,
+    freq = 'daily',
+    sort?: string,
+    columns?: string[],
+  ): Promise<Response> {
     const metaDataUrl: string = `/daily/${ticker}`;
     const latestPriceUrl: string = `/daily/${ticker}/prices`;
 
-    const json = fetch(this.base_url + latestPriceUrl, {
+    const response = await fetch(this._base_url + latestPriceUrl, {
       method: "GET",
       headers: this.headers,
     });
-
-    json.then((response) => {
-      return response.json();
-    }).then((jsonData) => {
-      console.log(typeof (jsonData));
-    });
-
-    // if (!response.ok) {
-    //   throw new HTTPError(`Fetch error: ${response.statusText}`);
-    // }
-    //   console.log(await response.json());
-    // return response.json();
+    return response;
   }
 }

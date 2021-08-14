42fe46ac0f4c07f077ed00caf9a89808ec50c7f7
Brady Bellini <bbellini@pm.me>
2021-08-13 22:20:08 -0700

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -2,4 +2,12 @@ import { Tiingo } from "../src/api.ts";
 
 const tiingo = new Tiingo("123");
 
-tiingo.eod("HOOD");
+// tiingo.eod("HOOD").then(
+//   (jsonData: any) => console.log(jsonData)
+// );
+
+// tiingo.eod("HOOD").then(data => console.log(data[0]['high']))
+
+let hood = await tiingo.eod('HOOD')
+
+console.log(await hood.json().then(data => data))

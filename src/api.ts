5c6d02b87e9d8b359020912ffd963e0cc39121ea
Brady Bellini <bbellini@pm.me>
2021-08-14 21:33:21 -0700

    Update api.ts
    added an internal fetch/get request function to reuse and more eod parameters that supports the api

--- src/api.ts
+++ src/api.ts
@@ -1,35 +1,57 @@
-class HTTPError extends Error {}
 export class Tiingo {
   private _base_url: string;
-  public headers: { [name: string]: string };
-  public apiKey: string;
+  private headers: { [name: string]: string };
+  private apiKey: string;
 
   constructor(apiKey: string) {
     this._base_url = "https://api.tiingo.com/tiingo";
-    this.apiKey = `&token=${apiKey}`;
+
+    this.apiKey = apiKey;
+
     this.headers = {
-      "Authorization": `Token ${apiKey}`,
+      "Authorization": `Token ${this.apiKey}`,
       "Content-Type": "application/json",
     };
   }
-// work on constructing the request with the optional variables
-  // possibly add logic for sorting
-  // TODO add error catching maybe
-  async eod(
-    ticker: string,
-    startDate?: string,
-    endDate?: string,
-    freq = 'daily',
-    sort?: string,
-    columns?: string[],
-  ): Promise<Response> {
-    const metaDataUrl: string = `/daily/${ticker}`;
-    const latestPriceUrl: string = `/daily/${ticker}/prices`;
 
-    const response = await fetch(this._base_url + latestPriceUrl, {
+  async _getRequest(url: string): Promise<Response> {
+    const response = await fetch(url, {
       method: "GET",
       headers: this.headers,
     });
+    
     return response;
+  }
+
+  // possibly add logic for sorting
+  // TODO add error catching maybe
+  async eod(
+    { ticker, startDate, endDate, freq = "daily", sort, columns }: {
+      ticker: string;
+      startDate?: string;
+      endDate?: string;
+      freq?: string;
+      sort?: string;
+      columns?: string[];
+    },
+  ): Promise<Response> {
+    const metaDataUrl: string = `/daily/${ticker}`;
+
+    let latestPriceUrl: string =
+      `/daily/${ticker}/prices?resampleFreq=${freq}&`;
+
+    if (startDate) {
+      latestPriceUrl += `startDate=${startDate}&`;
+    }
+
+    if (endDate) {
+      latestPriceUrl += `endDate=${endDate}&`;
+    }
+
+    if (columns) {
+      latestPriceUrl += `columns=${columns}`;
+    }
+
+    return this._getRequest(this._base_url + latestPriceUrl);
   }
 }

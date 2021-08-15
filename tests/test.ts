6a520af055093e281cb90ff05c707cc5db4cf84d
Brady Bellini <bbellini@pm.me>
2021-08-14 21:32:16 -0700

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -8,6 +8,12 @@ const tiingo = new Tiingo("123");
 
 // tiingo.eod("HOOD").then(data => console.log(data[0]['high']))
 
-let hood = await tiingo.eod('HOOD')
+let hood = await tiingo.eod({
+  ticker: "HOOD",
+  freq: "weekly",
+  startDate: "2021-08-01",
+  endDate: "2021-08-14",
+  columns: ["high", "low"],
+});
 
-console.log(await hood.json().then(data => data))
+console.log(await hood.json());

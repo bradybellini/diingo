952a761763a5d1ad5f6d9ba7717fc27b51b74c82
Brady Bellini <bbellini@pm.me>
2022-04-04 18:01:26 -0700

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { Tiingo } from "../src/api.ts";
 
-const tiingo = new Tiingo("123");
+const tiingo = new Tiingo("f1f892baaa285cb176acd888cba9d29cac2db57f");
 
 // tiingo.eod("HOOD").then(
 //   (jsonData: any) => console.log(jsonData)
@@ -8,12 +8,16 @@ const tiingo = new Tiingo("123");
 
 // tiingo.eod("HOOD").then(data => console.log(data[0]['high']))
 
-// const nvda = await tiingo.eod({
-//   ticker: "NVDA",
-// });
+const nvda = await tiingo.eod({
+  ticker: "SCHD",
+});
 
-// console.log(await nvda.json());
+console.log(await nvda.json());
 
-const nvda = await tiingo.ticker_meta('NVDA');
+// const nvda = await tiingo.ticker_meta('NVDA');
 
-console.log(await nvda.json().then(data => (data['description'])))
+// console.log(await nvda.json().then(data => (data['description'])))
+
+// const news = await tiingo.latestNews();
+
+// console.log(await news.json());
